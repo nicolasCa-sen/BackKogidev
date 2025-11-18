f2a989cd1f76db983bdcbe0290fa7bf989f81138
Luiszeror <luis.robelto@uptc.edu.co>
2025-11-18 17:37:06 -0500

    hola :c

--- api/index.js
+++ api/index.js
@@ -3,9 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const serverless = require('serverless-http');
 const connectDB = require('../config/db');
-const app = require('../app');
 
-// Rutas
 const authRoutes = require('../routes/auth');
 const insumoRoutes = require('../routes/insumos');
 const movementRoutes = require('../routes/movements');
@@ -20,16 +18,8 @@ app.use(cors());
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
-// Conectarse a Mongo Atlas
-(async () => {
-  try {
-    await connectDB(process.env.MONGO_URI);
-  } catch (error) {
-    console.error("Error connecting to MongoDB:", error);
-  }
-})();
+connectDB(process.env.MONGO_URI).catch(console.error);
 
-// Rutas API
 app.use('/api/auth', authRoutes);
 app.use('/api/insumos', insumoRoutes);
 app.use('/api/movements', movementRoutes);
@@ -38,10 +28,8 @@ app.use('/api/sales', salesRoutes);
 app.use('/api/products', productsRoutes);
 app.use('/api/ceramics', ceramicsRoutes);
 
-// Ruta de prueba
 app.get('/', (req, res) => {
-  res.send('API Cafe Galería - Backend operativo en Vercel');
+  res.send('API Cafe Galería - Backend operativo');
 });
 
-// ❗ OJO: NO usar app.listen() en Vercel
 module.exports = serverless(app);
